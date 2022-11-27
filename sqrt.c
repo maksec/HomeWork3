b3c9d88b080394fda25b3f6fcb16ffc95a6c5519
maxec <68388301+maksec@users.noreply.github.com>
2022-11-28 00:32:04 +0300

    Add files via upload

--- sqrt.c
+++ sqrt.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     double a;
     scanf("%lf", &a);
     double ans = 1000.0;
-    for (int i = 0; i < 52; ++i) {
+    while (fabs((ans * ans) / a - 1) >= 0.00000025) {
         ans = ans / 2 + (a / ans / 2);
     }
     printf("%lf", ans);
